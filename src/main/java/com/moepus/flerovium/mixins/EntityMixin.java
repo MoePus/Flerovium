51659b680dcb778f4e423b673d583079f185ca09
MoePus <547007249@qq.com>
2024-11-19 21:32:39 +0900

    Add priority 100 to make compatible with lithium

--- src/main/java/com/moepus/flerovium/mixins/EntityMixin.java
+++ src/main/java/com/moepus/flerovium/mixins/EntityMixin.java
@@ -1,14 +1,11 @@
 package com.moepus.flerovium.mixins;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.SoftOverride;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -16,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-@Mixin(value = Entity.class)
+@Mixin(value = Entity.class, priority = 100)
 public abstract class EntityMixin {
     @Shadow
     public abstract Level level();
