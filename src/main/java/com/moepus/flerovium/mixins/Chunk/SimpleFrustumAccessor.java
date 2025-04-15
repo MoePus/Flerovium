cf4380dd0874a1864e819e1d791c5e040183a9e0
MoePus <547007249@qq.com>
2025-04-15 22:12:48 +0900

    remap = false

--- src/main/java/com/moepus/flerovium/mixins/Chunk/SimpleFrustumAccessor.java
+++ src/main/java/com/moepus/flerovium/mixins/Chunk/SimpleFrustumAccessor.java
@@ -5,7 +5,7 @@ import org.joml.FrustumIntersection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(SimpleFrustum.class)
+@Mixin(value = SimpleFrustum.class, remap = false)
 public interface SimpleFrustumAccessor {
     @Accessor
     FrustumIntersection getFrustum();
--- src/main/java/com/moepus/flerovium/mixins/Chunk/ViewportMixin.java
+++ src/main/java/com/moepus/flerovium/mixins/Chunk/ViewportMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Viewport.class)
+@Mixin(value = Viewport.class, remap = false)
 public abstract class ViewportMixin implements FastViewport {
     @Unique
     private FastSimpleFrustum flerovium$frustum = null;
